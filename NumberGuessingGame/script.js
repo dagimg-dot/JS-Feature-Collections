2a33967323cb712cc4980ad4489e27ef70b27321
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-09-10 16:21:06 +0300

    make responses more attractive

--- NumberGuessingGame/script.js
+++ NumberGuessingGame/script.js
@@ -1,17 +1,26 @@
 const rand = Math.round(Math.random() * 10);
 const output = document.querySelector(".output");
 const btn = document.querySelector(".guess-btn");
+const guess = document.querySelector("input");
+
+let count = 0;
 
 guessButtonHandler = () => {
-  const guess = document.querySelector("input");
-  if (rand == Number(guess.value)) {
-    output.innerHTML = "You are correct";
-    output.style.color = "green";
+  count++;
+  if (count == 3) {
+    console.log("3 click");
   } else {
-    output.innerHTML = "Try again";
-    output.style.color = "red";
-    guess.value = "";
+    const guess = document.querySelector("input");
+    if (rand == Number(guess.value)) {
+      output.innerHTML = "<span>😁</span><h1>You are correct</h1>";
+      output.style.color = "green";
+    } else {
+      output.innerHTML = "<span>😒</span><h1>Try again</h1>";
+      output.style.color = "red";
+      guess.value = "";
+    }
   }
+  guess.focus();
 };
 
 btn.addEventListener("click", guessButtonHandler);
